8e619a92ec481665508354b8a2e7346f24b82aab
TRING Philippe <ptring@p-212b-03.intra.gobelins.fr>
2014-09-25 10:05:38 +0200

    move windows config to another file

--- .vagrant_bootstrap/custom.sh
+++ .vagrant_bootstrap/custom.sh
@@ -29,30 +29,5 @@ service apache2 reload
 # Add config for xDebug
 echo "xdebug.max_nesting_level = 250" >> /etc/php5/apache2/php.ini
 
-#################### FOR WINDOWS ONLY
-# Remove NFS in that case in VagrantFile
-# After that create "nouveau lecteur réseau" by clicking right on Computer. Add \\192.168.100.10\shared as the server log/password are vagrant/vagrant
-
-# # Installing Samba
-# SAMBA_USER="vagrant"
-# SAMBA_PASSWORD="vagrant"
-# apt-get install -y samba
-# service smbd stop
-# # Configure Samba
-# rm -rf /etc/samba/smb.conf
-# cp /vagrant/.configs/samba/smb.conf.dist /etc/samba/smb.conf
-# service smbd start
-# # Configure user
-# echo -ne "$SAMBA_PASSWORD\n$SAMBA_PASSWORD\n" | smbpasswd -L -a $SAMBA_USER
-# smbpasswd -L -e $SAMBA_USER
- 
-# # Finally, give all rights
-# chmod 777 -R /var/www
-# chown -R $SAMBA_USER:$SAMBA_USER
-# # Setting apache2 user and group to samba user
-# sed "s#export APACHE_RUN_USER=.*#export APACHE_RUN_USER=$SAMBA_USER#g" /etc/apache2/envvars > /etc/apache2/envvars.tmp
-# mv /etc/apache2/envvars.tmp /etc/apache2/envvars
-# sed "s#export APACHE_RUN_GROUP=.*#export APACHE_RUN_GROUP=$SAMBA_USER#g" /etc/apache2/envvars > /etc/apache2/envvars.tmp
-# mv /etc/apache2/envvars.tmp /etc/apache2/envvars
-
-######################## END FOR WINDOWS ONLY
+#For windows only few manipulation to do check them in windows.sh
+#source /vagrant/.vagrant_bootstrap/windows.sh
